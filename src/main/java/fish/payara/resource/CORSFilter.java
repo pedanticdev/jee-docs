1486620f51493d59adfd2c3e3105a3234f82c7a8
Luqman Saeed <luqman.saeed@payara.fish>
2025-04-16 09:35:58 +0000

    add html pages directly from reach app

--- src/main/java/fish/payara/resource/CORSFilter.java
+++ src/main/java/fish/payara/resource/CORSFilter.java
@@ -32,15 +32,6 @@ public class CORSFilter implements ContainerResponseFilter {
 
         Logger.getLogger(CORSFilter.class.getSimpleName()).log(Level.INFO, "CORS filter enabled. Allowed origins: {0}", allowedOrigins);
         final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-//        headers.add("Access-Control-Allow-Origin", allowedOrigins);
-//        headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin, Accept, Authorization, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-//        headers.add("Access-Control-Expose-Headers", getRequestedExposedHeaders(requestContext));
-//        headers.add("Access-Control-Allow-Credentials", "true");
-//        headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-//        headers.add("Access-Control-Max-Age", MAX_AGE);
-//        headers.add("x-responded-by", "cors-response-filter");
-//        responseContext.setStatus(Response.Status.OK.getStatusCode());
-
         // Handle preflight
         if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
             headers.add("Access-Control-Allow-Origin", allowedOrigins);
